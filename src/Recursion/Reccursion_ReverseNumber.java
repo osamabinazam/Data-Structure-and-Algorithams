5ea2395ef2d2a14431aff9166febe66166fbb3d2
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-07 22:01:34 +0500

    .

---
+++ src/Recursion/Reccursion_ReverseNumber.java
@@ -0,0 +1,18 @@
+
+
+
+import java.util.*;
+public class Recursion_ReverseNumber{
+    public static void main (String [] args ){
+
+
+    }
+
+    public static int reverseNumber(int source ){
+
+
+        
+    }
+
+
+}
