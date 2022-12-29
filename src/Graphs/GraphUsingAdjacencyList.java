e073f6870c6fe43d7736863a70a4ea05dfa3e3aa
Osama Bin Azam <osamabinazam@gmail.com>
2022-12-29 17:10:31 +0500

    Implement DFS

--- src/Graphs/GraphUsingAdjacencyList.java
+++ src/Graphs/GraphUsingAdjacencyList.java
@@ -1,24 +1,27 @@
 package Graphs;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class GraphUsingAdjacencyList {
 
     //Class Members
-    ArrayList<GraphNode> list = new ArrayList<>();
+    ArrayList<GraphNode> orgList = new ArrayList<>();
+    ArrayList<GraphNode> list = (ArrayList<GraphNode>) orgList.clone();
+    
+    
 
     //Constructor
      GraphUsingAdjacencyList(ArrayList<GraphNode> list){
-        this.list=list;
+        this.orgList=list;
      }
     
     //Method that build Undirected Graph
     public void addUndirectedGraph(int i , int j){
-        GraphNode first = list.get(i);
-        GraphNode second = list.get(j);
+        GraphNode first = orgList.get(i);
+        GraphNode second = orgList.get(j);
         first.neighbors.add(second);
         second.neighbors.add(first); 
-
     } 
     /*************************************************/
     /*              Implementing BFS Traversal       */
@@ -49,8 +52,39 @@ public class GraphUsingAdjacencyList {
             if(!graphNode.isVisited)
                     visitedBFS(graphNode);
         }
+        
     }
 
+
+    /*************************************************/
+    /*              Implementing DFS Traversal       */
+    /*************************************************/
+
+    //Helper Method of Original DFS
+
+    private void visitedDFS(GraphNode node){
+        Stack <GraphNode> stack = new Stack<>();
+        stack.push(node);
+        while(!stack.isEmpty()){
+            GraphNode currentNode = stack.pop();
+            currentNode.isVisited=true;
+            System.out.print(currentNode.name + " ");
+            for (GraphNode graphNode : currentNode.neighbors) {
+                if(!graphNode.isVisited)
+                    stack.push(graphNode);
+                    graphNode.isVisited=true;
+            }
+        }
+    }
+
+    //Driver Method of Depth First Search
+    public void DFS(){
+        for (GraphNode graphNode : list) {
+                if(!graphNode.isVisited)
+                        visitedDFS(graphNode);
+        }      
+        list=orgList;     
+    }
 
 
 
@@ -58,13 +92,13 @@ public class GraphUsingAdjacencyList {
     public String toString(){
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < list.size(); i++) {
-            str.append(list.get(i).name +": ");
+            str.append(orgList.get(i).name +": ");
             for (int j = 0; j < list.get(i).neighbors.size(); j++) {
-                if (j == list.get(i).neighbors.size() -1 ){
-                    str.append(list.get(i).neighbors.get(j).name);
+                if (j == orgList.get(i).neighbors.size() -1 ){
+                    str.append(orgList.get(i).neighbors.get(j).name);
                 }
                 else{
-                    str.append(list.get(i).neighbors.get(j).name + "->");
+                    str.append(orgList.get(i).neighbors.get(j).name + "->");
                 }
             }
 
@@ -97,7 +131,11 @@ public class GraphUsingAdjacencyList {
 
         //BFS Traversal
         System.out.println("Start Traversing");
+        System.out.println("Breadth First Search");
         graph.BFS();
+
+        System.out.println("\nDepth First Search");
+        graph.DFS();
 
     }
     
