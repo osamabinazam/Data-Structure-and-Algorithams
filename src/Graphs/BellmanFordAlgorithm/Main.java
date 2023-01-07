5aa95a5b864fcbac8749a456862f2b5ec75a6946
Osama Bin Azam <osamabinazam@gmail.com>
2023-01-07 23:56:53 +0500

    Driver For Bellman Ford

--- src/Graphs/BellmanFordAlgorithm/Main.java
+++ src/Graphs/BellmanFordAlgorithm/Main.java
@@ -17,6 +17,18 @@ public class Main {
         graph.add(new WeightedNode("E",4));
         graph.add(new WeightedNode("F",5));
         graph.add(new WeightedNode("G",7));
+        WeightedGraph weightedGraph = new WeightedGraph(graph);
+        weightedGraph.addWeightedNode(0,1,2);
+        weightedGraph.addWeightedNode(0,2,5);
+        weightedGraph.addWeightedNode(1,2,6);
+        weightedGraph.addWeightedNode(1,3,1);
+        weightedGraph.addWeightedNode(1,4,3);
+        weightedGraph.addWeightedNode(2,5,8);
+        weightedGraph.addWeightedNode(2,4,4);
+        weightedGraph.addWeightedNode(4,6,9);
+        weightedGraph.addWeightedNode(5,6,7);
+        System.out.println("Printing Bellman Ford Algorithm");
+        weightedGraph.bellmanFord(graph.get(0));
     }
     
 }
