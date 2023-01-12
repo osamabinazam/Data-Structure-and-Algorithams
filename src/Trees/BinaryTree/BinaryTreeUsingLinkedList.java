fc616a9d566fc4ab69fc7efc890eb4dc463de19d
Osama Bin Azam <osamabinazam@gmail.com>
2023-01-12 23:49:04 +0500

    Created Binary Tree Using Linked List

---
+++ src/Trees/BinaryTree/BinaryTreeUsingLinkedList.java
@@ -0,0 +1,11 @@
+package Trees.BinaryTree;
+
+public class BinaryTreeUsingLinkedList {
+    Node root; //root node of the tree
+
+    //Constructor
+    BinaryTreeUsingLinkedList(){
+        this.root=null;
+    }
+    
+}
