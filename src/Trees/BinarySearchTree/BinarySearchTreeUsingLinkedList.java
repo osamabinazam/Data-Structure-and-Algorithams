811dd340570ac6dccbb32c0bf62e171ab0c501c0
Osama Bin Azam <osamabinazam@gmail.com>
2023-01-14 12:04:51 +0500

    Created BinarySearchTree

---
+++ src/Trees/BinarySearchTree/BinarySearchTreeUsingLinkedList.java
@@ -0,0 +1,18 @@
+package Trees.BinarySearchTree;
+
+import Graphs.BellmanFordAlgorithm.Main;
+
+public class BinarySearchTreeUsingLinkedList {
+
+    Node rootNode;
+    BinarySearchTreeUsingLinkedList(){
+        rootNode = null;
+    }
+
+    
+    public static void main(String[] args) {
+        BinarySearchTreeUsingLinkedList bt = new BinarySearchTreeUsingLinkedList();
+    }
+
+}
+
